2aa933cb420d37f4b6fdcc090dd6a125e7248b4d
robotbrain <robotbrainify@gmail.com>
2019-07-28 19:01:20 -0400

    Heck loom.

--- src/main/java/robosky/ether/clientmixins/BGRenderMixin.java
+++ src/main/java/robosky/ether/clientmixins/BGRenderMixin.java
@@ -2,10 +2,12 @@ package robosky.ether.clientmixins;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import robosky.ether.MixinHack;
 
@@ -25,5 +27,10 @@ public class BGRenderMixin {
             scale = 1.0;
         }
         return scale;
+    }
+
+    @Inject(at = @At("HEAD"), method = "renderBackground")
+    private void heckLoom(Camera unused1, float unused2) {
+
     }
 }
