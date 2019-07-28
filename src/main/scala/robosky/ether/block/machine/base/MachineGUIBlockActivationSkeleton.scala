6daa73523b8343f77e4a98f11991b87a7db59767
robotbrain <robotbrainify@gmail.com>
2019-07-28 01:17:32 -0400

    Naming and fix compiler errors

--- src/main/scala/robosky/ether/block/machine/base/MachineGUIBlockActivationSkeleton.scala
+++ src/main/scala/robosky/ether/block/machine/base/MachineGUIBlockActivationSkeleton.scala
@@ -1,25 +1,33 @@
 package robosky.ether.block.machine.base
 
+import java.util.function.Consumer
+
 import net.fabricmc.fabric.api.container.ContainerProviderRegistry
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.{Block, BlockState}
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.registry.Registry
+import net.minecraft.util.{Hand, PacketByteBuf}
 import net.minecraft.world.World
 
-trait MachineGUIBlockActivationSkeleton[A <: BlockEntity[B], B <: Block] {
-  self: B =>
+import scala.reflect.ClassTag
+
+trait MachineGUIBlockActivationSkeleton[A <: BlockEntity] {
+  self: Block =>
 
   override def activate(state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hitResult: BlockHitResult): Boolean = {
     if (world.isClient) return true
     world.getBlockEntity(pos) match {
-      case _: A =>
-        ContainerProviderRegistry.INSTANCE.openContainer(Registry.BLOCK.getId(self), player, buf => buf.writeBlockPos(pos))
+      case a if beCTag.runtimeClass.isInstance(a) =>
+        ContainerProviderRegistry.INSTANCE.openContainer(Registry.BLOCK.getId(self), player, new Consumer[PacketByteBuf] {
+          override def accept(buf: PacketByteBuf): Unit = buf.writeBlockPos(pos)
+        })
       case _ =>
     }
     true
   }
+
+  protected def beCTag: ClassTag[A]
 }
