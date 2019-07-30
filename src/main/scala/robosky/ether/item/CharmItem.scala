bec8deed18bcfb6451a0a02db6521609e7b0f3de
robotbrain <robotbrainify@gmail.com>
2019-07-30 15:44:59 -0400

    Fix charms

--- src/main/scala/robosky/ether/item/CharmItem.scala
+++ src/main/scala/robosky/ether/item/CharmItem.scala
@@ -9,7 +9,8 @@ import robosky.ether.{TickableItem, UplandsMod}
 case class CharmItem(effect: StatusEffect, level: Int, maxDurability: Int = 2400) extends Item(new Item.Settings()
   .group(UplandsMod.GROUP).maxCount(1).maxDamage(maxDurability)) with TickableItem {
   override def tick(entity: PlayerEntity, stack: ItemStack, hand: Hand): Unit = {
-    stack.damage(1, entity, (player: PlayerEntity) => player.addPotionEffect(new StatusEffectInstance(
-      effect, 10, level, false, false, false)))
+    stack.damage(1, entity, (player: PlayerEntity) => {})
+    entity.addPotionEffect(new StatusEffectInstance(effect, 10, level, false, false,
+      false))
   }
 }
