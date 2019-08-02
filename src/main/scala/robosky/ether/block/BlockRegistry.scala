c113d9e3e5fa69f629397c93a63d641b4ebe5970
void <theprismaticvoid@gmail.com>
2019-08-01 19:40:06 -0500

    All Skyroot items can now be used as furnace fuel

--- src/main/scala/robosky/ether/block/BlockRegistry.scala
+++ src/main/scala/robosky/ether/block/BlockRegistry.scala
@@ -1,7 +1,7 @@
 package robosky.ether.block
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings
-import net.fabricmc.fabric.api.registry.{CompostingChanceRegistry, FlammableBlockRegistry}
+import net.fabricmc.fabric.api.registry.{CompostingChanceRegistry, FlammableBlockRegistry, FuelRegistry}
 import net.fabricmc.fabric.api.tools.FabricToolTags
 import net.minecraft.block._
 import net.minecraft.item.{BlockItem, Item, ItemGroup}
@@ -17,17 +17,17 @@ object BlockRegistry {
 
     val ETHER_STONE_SETTINGS: Block.Settings = FabricBlockSettings.of(Material.STONE)
         .strength(1.5f, 6f).sounds(BlockSoundGroup.STONE)
-        .breakByTool(FabricToolTags.PICKAXES, 1).build;
+        .breakByTool(FabricToolTags.PICKAXES, 1).build
 
-    val ETHER_STONE: Block = register("ether_stone")(new Block(ETHER_STONE_SETTINGS));
-    val ETHER_STONE_STAIRS: StairsBlock = register("ether_stone_stairs")(new ModStairsBlock(ETHER_STONE, ETHER_STONE_SETTINGS));
-    val ETHER_STONE_SLAB: SlabBlock = register("ether_stone_slab")(new SlabBlock(ETHER_STONE_SETTINGS));
-    val ETHER_STONE_WALL: WallBlock = register("ether_stone_wall")(new WallBlock(ETHER_STONE_SETTINGS));
+    val ETHER_STONE: Block = register("ether_stone")(new Block(ETHER_STONE_SETTINGS))
+    val ETHER_STONE_STAIRS: StairsBlock = register("ether_stone_stairs")(new ModStairsBlock(ETHER_STONE, ETHER_STONE_SETTINGS))
+    val ETHER_STONE_SLAB: SlabBlock = register("ether_stone_slab")(new SlabBlock(ETHER_STONE_SETTINGS))
+    val ETHER_STONE_WALL: WallBlock = register("ether_stone_wall")(new WallBlock(ETHER_STONE_SETTINGS))
 
-    val ETHER_STONE_BRICKS: Block = register("ether_stone_bricks")(new Block(ETHER_STONE_SETTINGS));
-    val ETHER_STONE_BRICK_STAIRS: StairsBlock = register("ether_stone_brick_stairs")(new ModStairsBlock(ETHER_STONE_BRICKS, ETHER_STONE_SETTINGS));
-    val ETHER_STONE_BRICK_SLAB: SlabBlock = register("ether_stone_brick_slab")(new SlabBlock(ETHER_STONE_SETTINGS));
-    val ETHER_STONE_BRICK_WALL: WallBlock = register("ether_stone_brick_wall")(new WallBlock(ETHER_STONE_SETTINGS));
+    val ETHER_STONE_BRICKS: Block = register("ether_stone_bricks")(new Block(ETHER_STONE_SETTINGS))
+    val ETHER_STONE_BRICK_STAIRS: StairsBlock = register("ether_stone_brick_stairs")(new ModStairsBlock(ETHER_STONE_BRICKS, ETHER_STONE_SETTINGS))
+    val ETHER_STONE_BRICK_SLAB: SlabBlock = register("ether_stone_brick_slab")(new SlabBlock(ETHER_STONE_SETTINGS))
+    val ETHER_STONE_BRICK_WALL: WallBlock = register("ether_stone_brick_wall")(new WallBlock(ETHER_STONE_SETTINGS))
 
     val ETHER_ORES: Map[EtherOreBlock.EtherOreType, EtherOreBlock] = EtherOreBlock.oreTypes.map(t => t ->
         register(s"${t.name}_ore")(new EtherOreBlock(t))).toMap
@@ -35,19 +35,34 @@ object BlockRegistry {
     val skyroot: Block.Settings = FabricBlockSettings.of(Material.WOOD, MaterialColor.BROWN)
         .strength(2.0F, 2.0F).sounds(BlockSoundGroup.WOOD)
         .breakByTool(FabricToolTags.AXES, -1).build
+
     val SKYROOT_LOG: LogBlock = register("skyroot_log")(new LogBlock(MaterialColor.WOOD, skyroot))
-    val SKYROOT_WOOD: LogBlock = register("skyroot_wood")(new LogBlock(MaterialColor.WOOD, skyroot));
+    FuelRegistry.INSTANCE.add(SKYROOT_LOG, 300)
+
+    val SKYROOT_WOOD: LogBlock = register("skyroot_wood")(new LogBlock(MaterialColor.WOOD, skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_WOOD, 300)
+
     val SKYROOT_PLANKS: Block = register("skyroot_planks")(new Block(skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_PLANKS, 300)
+
     val SKYROOT_SLAB: SlabBlock = register("skyroot_slab")(new SlabBlock(skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_SLAB, 150)
+
     val SKYROOT_STAIRS: StairsBlock = register("skyroot_stairs")(new ModStairsBlock(SKYROOT_PLANKS, skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_STAIRS, 300)
+
     val SKYROOT_FENCE: FenceBlock = register("skyroot_fence")(new FenceBlock(skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_FENCE, 300)
+
     val SKYROOT_FENCE_GATE: FenceGateBlock = register("skyroot_fence_gate")(new FenceGateBlock(skyroot))
+    FuelRegistry.INSTANCE.add(SKYROOT_FENCE_GATE, 300)
 
     val SKYROOT_SAPLING: EtherSaplingBlock = register("skyroot_sapling")(new EtherSaplingBlock(
         EtherSaplingGenerator.SkyrootSaplingGenerator, FabricBlockSettings.of(Material.PLANT).noCollision.ticksRandomly
             .breakInstantly.sounds(BlockSoundGroup.GRASS).build()))
 
-    CompostingChanceRegistry.INSTANCE.add(SKYROOT_SAPLING, 0.65f);
+    CompostingChanceRegistry.INSTANCE.add(SKYROOT_SAPLING, 0.65f)
+    FuelRegistry.INSTANCE.add(SKYROOT_WOOD, 100)
 
     val skyrootLeavesSettings: Block.Settings = FabricBlockSettings
         .of(Material.LEAVES)
@@ -55,24 +70,25 @@ object BlockRegistry {
         .ticksRandomly
         .sounds(BlockSoundGroup.GRASS).build()
 
-    val RED_SKYROOT_LEAVES: LeavesBlock = register("red_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings));
-    val ORANGE_SKYROOT_LEAVES: LeavesBlock = register("orange_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings));
-    val YELLOW_SKYROOT_LEAVES: LeavesBlock = register("yellow_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings));
+    val RED_SKYROOT_LEAVES: LeavesBlock = register("red_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings))
+    CompostingChanceRegistry.INSTANCE.add(RED_SKYROOT_LEAVES, 0.30f)
 
-    CompostingChanceRegistry.INSTANCE.add(RED_SKYROOT_LEAVES, 0.30f);
-    CompostingChanceRegistry.INSTANCE.add(ORANGE_SKYROOT_LEAVES, 0.30f);
-    CompostingChanceRegistry.INSTANCE.add(YELLOW_SKYROOT_LEAVES, 0.30f);
+    val ORANGE_SKYROOT_LEAVES: LeavesBlock = register("orange_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings))
+    CompostingChanceRegistry.INSTANCE.add(ORANGE_SKYROOT_LEAVES, 0.30f)
 
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_LOG, 5, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_WOOD, 5, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_PLANKS, 20, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_SLAB, 20, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_STAIRS, 20, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(RED_SKYROOT_LEAVES, 60, 30);
-    FlammableBlockRegistry.getDefaultInstance().add(ORANGE_SKYROOT_LEAVES, 60, 30);
-    FlammableBlockRegistry.getDefaultInstance().add(YELLOW_SKYROOT_LEAVES, 60, 30);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_FENCE, 20, 5);
-    FlammableBlockRegistry.getDefaultInstance().add(SKYROOT_FENCE_GATE, 20, 5);
+    val YELLOW_SKYROOT_LEAVES: LeavesBlock = register("yellow_skyroot_leaves")(new LeavesBlock(skyrootLeavesSettings))
+    CompostingChanceRegistry.INSTANCE.add(YELLOW_SKYROOT_LEAVES, 0.30f)
+
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_LOG, 5, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_WOOD, 5, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_PLANKS, 20, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_SLAB, 20, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_STAIRS, 20, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(RED_SKYROOT_LEAVES, 60, 30)
+    FlammableBlockRegistry.getDefaultInstance.add(ORANGE_SKYROOT_LEAVES, 60, 30)
+    FlammableBlockRegistry.getDefaultInstance.add(YELLOW_SKYROOT_LEAVES, 60, 30)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_FENCE, 20, 5)
+    FlammableBlockRegistry.getDefaultInstance.add(SKYROOT_FENCE_GATE, 20, 5)
 
     val ETHER_BEACON: EtherBeaconBlock.type = register("ether_beacon")(EtherBeaconBlock)
 
