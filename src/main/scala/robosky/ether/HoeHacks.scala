7e23fdf8e287ebe7604f35bac19ba1ec82b808ef
robotbrain <robotbrainify@gmail.com>
2019-08-03 02:58:23 -0400

    whoops

--- src/main/scala/robosky/ether/HoeHacks.scala
+++ src/main/scala/robosky/ether/HoeHacks.scala
@@ -1,8 +1,8 @@
 package robosky.ether
 
 import net.minecraft.block.{Block, BlockState}
-import net.minecraft.item.HoeItem
+import net.minecraft.item.{HoeItem, Item, ToolMaterials}
 
-object HoeHacks extends HoeItem(null, 0f, null) {
+object HoeHacks extends HoeItem(ToolMaterials.WOOD, 0f, new Item.Settings()) {
   def addHoeable(source: Block, tgt: BlockState): BlockState = HoeItem.TILLED_BLOCKS.put(source, tgt)
 }
