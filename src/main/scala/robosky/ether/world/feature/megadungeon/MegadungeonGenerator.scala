61e1da7098af249c0c33641b7574844458fb229b
robotbrain <robotbrainify@gmail.com>
2019-08-05 01:35:57 -0400

    Add tentative metadata handling support

--- src/main/scala/robosky/ether/world/feature/megadungeon/MegadungeonGenerator.scala
+++ src/main/scala/robosky/ether/world/feature/megadungeon/MegadungeonGenerator.scala
@@ -1,15 +1,20 @@
 package robosky.ether.world.feature.megadungeon
 
 import java.util
+import java.util.Random
 
 import com.google.common.collect.ImmutableList
 import com.mojang.datafixers.util.Pair
+import net.minecraft.block.Blocks
+import net.minecraft.block.entity.ChestBlockEntity
+import net.minecraft.block.enums.StructureBlockMode
 import net.minecraft.structure.pool.StructurePool.Projection
 import net.minecraft.structure.pool.StructurePoolBasedGenerator.PieceFactory
 import net.minecraft.structure.pool._
-import net.minecraft.structure.{StructureManager, StructurePiece}
-import net.minecraft.util.Identifier
-import net.minecraft.util.math.BlockPos
+import net.minecraft.structure.{Structure, StructureManager, StructurePiece}
+import net.minecraft.util.math.{BlockPos, MutableIntBoundingBox}
+import net.minecraft.util.{BlockRotation, Identifier}
+import net.minecraft.world.IWorld
 import net.minecraft.world.gen.ChunkRandom
 import net.minecraft.world.gen.chunk.ChunkGenerator
 import robosky.ether.UplandsMod
@@ -23,7 +28,27 @@ object MegadungeonGenerator {
   }
 
   private def createSingleElement(name: String): StructurePoolElement = new SinglePoolElement((UplandsMod :/ name).toString,
-    ImmutableList.of(), Projection.RIGID)
+    ImmutableList.of(), Projection.RIGID) {
+
+    // Metadata shenanigans so your data structure blocks do something!
+    // The if statement in this method is probably unneeded based on the implementations of the methods that call this,
+    // but I included it for safety
+    override def method_16756(world: IWorld, info: Structure.StructureBlockInfo, pos: BlockPos, rotation: BlockRotation,
+      rand: Random, bbox: MutableIntBoundingBox): Unit =
+      if (info.tag != null && StructureBlockMode.valueOf(info.tag.getString("mode")) == StructureBlockMode.DATA)
+        handleMetadata(info.tag.getString("metadata"), info.pos, world, rand, bbox)
+  }
+
+
+  def handleMetadata(str: String, pos: BlockPos, world: IWorld, rand: Random, bbox: MutableIntBoundingBox): Unit = {
+    if (str.startsWith("loot!")) {
+      world.setBlockState(pos, Blocks.AIR.getDefaultState, 3)
+      world.getBlockEntity(pos.down) match {
+        case chest: ChestBlockEntity => chest.setLootTable(new Identifier(str.substring(5)), rand.nextLong)
+        case _ =>
+      }
+    }
+  }
 
   private def registerPool(name: String, pieces: (String, Int)*): Unit = {
     val array: Array[Pair[StructurePoolElement, Integer]] = pieces.map { case (s, i) => Pair.of(createSingleElement(s), Int.box(i)) }.toArray
