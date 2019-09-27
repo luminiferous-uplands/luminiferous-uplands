bad134c6e30da1bb9f3fff224bbbf62d915da2d2
robotbrain <robotbrainify@gmail.com>
2019-09-26 22:34:29 -0400

    Fix crash

--- src/main/java/robosky/ether/mixin/PlayerMixin.java
+++ src/main/java/robosky/ether/mixin/PlayerMixin.java
@@ -59,13 +59,15 @@ public abstract class PlayerMixin extends LivingEntity implements UplanderBeacon
 
     @Inject(method = "tickMovement", at = @At("TAIL"))
     private void onTickMovement(CallbackInfo info) {
-        if (this.dimension == WorldRegistry.UPLANDS_DIMENSION() && this.y <= -60) {
-            FabricDimensions.teleport(this, DimensionType.OVERWORLD, UplandsTeleporter.FromUplands$.MODULE$);
-        } else if (this.dimension == DimensionType.OVERWORLD && this.y >= 300.0) {
-            if (uplands_isUsingBeacon()) {
-                FabricDimensions.teleport(this, WorldRegistry.UPLANDS_DIMENSION(), UplandsTeleporter.ToUplandsBeacon$.MODULE$);
-            } else {
-                FabricDimensions.teleport(this, WorldRegistry.UPLANDS_DIMENSION(), UplandsTeleporter.ToUplandsFlying$.MODULE$);
+        if (!world.isClient) {
+            if (this.dimension == WorldRegistry.UPLANDS_DIMENSION() && this.y <= -60) {
+                FabricDimensions.teleport(this, DimensionType.OVERWORLD, UplandsTeleporter.FromUplands$.MODULE$);
+            } else if (this.dimension == DimensionType.OVERWORLD && this.y >= 300.0) {
+                if (uplands_isUsingBeacon()) {
+                    FabricDimensions.teleport(this, WorldRegistry.UPLANDS_DIMENSION(), UplandsTeleporter.ToUplandsBeacon$.MODULE$);
+                } else {
+                    FabricDimensions.teleport(this, WorldRegistry.UPLANDS_DIMENSION(), UplandsTeleporter.ToUplandsFlying$.MODULE$);
+                }
             }
         }
         if (uplands_isUsingBeacon()) {
