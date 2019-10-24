730e3819adfb8d35ffd8f5836690f92f75a4b7bb
Thalia Nero <chrisvn00@gmail.com>
2019-10-23 21:03:07 -0400

    Add hexahaen spawn to megadungeon

--- src/main/scala/robosky/uplands/entity/EntityRegistry.scala
+++ src/main/scala/robosky/uplands/entity/EntityRegistry.scala
@@ -3,6 +3,7 @@ package robosky.uplands.entity
 import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder
 
 import net.minecraft.entity.{EntityCategory, EntityDimensions, EntityType}
+import net.minecraft.item.{Item, ItemGroup, SpawnEggItem}
 import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 
@@ -15,7 +16,13 @@ object EntityRegistry {
     .size(EntityDimensions.fixed(1.25f, 1.25f))
     .build
 
+  private def registerMob(name: String, entityType: EntityType[_], primaryColor: Int, secondaryColor: Int): Unit = {
+    Registry.register(Registry.ENTITY_TYPE, UplandsMod :/ name, entityType)
+    Registry.register(Registry.ITEM, UplandsMod :/ (name + "_spawn_egg"),
+        new SpawnEggItem(entityType, primaryColor, secondaryColor, new Item.Settings().group(ItemGroup.MISC)))
+  }
+
   def init(): Unit = {
-    Registry.register(Registry.ENTITY_TYPE, UplandsMod :/ "hexahaen", HEXAHAEN);
+    registerMob("hexahaen", HEXAHAEN, 0x0e7543, 0xeb8154)
   }
 }
