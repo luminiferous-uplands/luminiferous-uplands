9d6d5ceaabb8782d40f716206ffae9dc6b9e6548
Thalia Nero <chrisvn00@gmail.com>
2019-11-26 23:35:31 -0500

    Fix water at max fall attempting to flow

---
+++ src/main/java/robosky/uplands/mixin/BaseFluidMixin.java
@@ -0,0 +1,34 @@
+package robosky.uplands.mixin;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.world.IWorld;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.fluid.BaseFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import robosky.uplands.block.UplandsWaterBlock;
+
+@Mixin(BaseFluid.class)
+public abstract class BaseFluidMixin extends Fluid {
+
+    /**
+     * Prevents fluid from trying to flow when its fall state is maximum.
+     */
+    @Inject(method = "flow", at = @At("HEAD"), cancellable = true)
+    private void tweakFlow(IWorld world, BlockPos pos, BlockState state, Direction dir, FluidState fluidState, CallbackInfo info) {
+        BlockState source = world.getBlockState(pos.offset(dir.getOpposite()));
+        if (source.getBlock() == Blocks.WATER && source.get(UplandsWaterBlock.FALL()) == UplandsWaterBlock.MAX_FALL()) {
+            info.cancel();
+        }
+    }
+}
