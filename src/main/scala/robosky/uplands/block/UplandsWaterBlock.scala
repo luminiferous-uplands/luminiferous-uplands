1f7f2205b735c4797f22a62d3684a806903d0809
Thalia Nero <chrisvn00@gmail.com>
2019-11-27 22:35:06 -0500

    40 blocks fall

--- src/main/scala/robosky/uplands/block/UplandsWaterBlock.scala
+++ src/main/scala/robosky/uplands/block/UplandsWaterBlock.scala
@@ -4,7 +4,7 @@ import net.minecraft.state.property.{Property, IntProperty}
 
 object UplandsWaterBlock {
 
-  val MAX_FALL: Int = 20
+  val MAX_FALL: Int = 40
 
   val FALL: Property[Integer] = IntProperty.of("fall", 0, MAX_FALL)
 }
