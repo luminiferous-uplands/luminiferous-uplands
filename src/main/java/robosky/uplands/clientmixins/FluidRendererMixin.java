e06638945072fd89de88a7f8d3e4a5a13712ef17
Thalia Nero <chrisvn00@gmail.com>
2019-11-30 21:14:12 -0500

    Add water fade render

--- src/main/java/robosky/uplands/clientmixins/FluidRendererMixin.java
+++ src/main/java/robosky/uplands/clientmixins/FluidRendererMixin.java
@@ -2,11 +2,14 @@ package robosky.uplands.clientmixins;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.block.FluidRenderer;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ExtendedBlockView;
+import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,6 +19,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import robosky.uplands.block.UplandsWaterBlock;
+import robosky.uplands.world.WorldRegistry;
 
 @Mixin(FluidRenderer.class)
 public abstract class FluidRendererMixin {
@@ -24,13 +28,21 @@ public abstract class FluidRendererMixin {
     private float alpha;
 
     @Inject(method = "tesselate", at = @At("HEAD"))
-    private void onTesselate(ExtendedBlockView world, BlockPos pos, BufferBuilder builder, FluidState state, CallbackInfoReturnable<Boolean> info) {
+    private void setUplandsWaterAlpha(ExtendedBlockView world, BlockPos pos, BufferBuilder builder,
+            FluidState state, CallbackInfoReturnable<Boolean> info) {
         // the number of blocks the fade takes up
         final float FADE_LENGTH = 10;
         alpha = 1.0f;
-        BlockState blockState = world.getBlockState(pos);
-        if (blockState.getBlock() == Blocks.WATER) {
-            int adjustedFall = UplandsWaterBlock.MAX_FALL() - blockState.get(UplandsWaterBlock.FALL());
+        if (state.getFluid().matchesType(Fluids.WATER)) {
+            int adjustedFall = UplandsWaterBlock.MAX_FALL();
+            BlockState blockState = world.getBlockState(pos);
+            if (blockState.getBlock() == Blocks.WATER) {
+                adjustedFall -= blockState.get(UplandsWaterBlock.FALL());
+            }
+            // fade out into the void as well
+            if (MinecraftClient.getInstance().world.getDimension().getType() == WorldRegistry.UPLANDS_DIMENSION()) {
+                adjustedFall = Math.min(adjustedFall, pos.getY());
+            }
             if (adjustedFall < FADE_LENGTH) {
                 alpha = (adjustedFall + 1) / (float)FADE_LENGTH;
             }
