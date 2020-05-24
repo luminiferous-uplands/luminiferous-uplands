bf42ac29f956451154ea2f573221c313dff0f607
Thalia Nero <chrisvn00@gmail.com>
2020-05-24 19:30:28 -0400

    Convert REI integration.

---
+++ src/main/java/robosky/uplands/interop/UplandsREIPlugin.java
@@ -0,0 +1,40 @@
+package robosky.uplands.interop;
+
+import me.shedaniel.rei.api.EntryStack;
+import me.shedaniel.rei.api.RecipeHelper;
+import me.shedaniel.rei.api.plugins.REIPluginV0;
+import robosky.uplands.UplandsMod;
+import robosky.uplands.block.machine.MachineRegistry;
+import robosky.uplands.block.machine.infuser.AegisaltRecipe;
+
+import net.minecraft.util.Identifier;
+
+import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.util.version.VersionParsingException;
+
+public final class UplandsREIPlugin implements REIPluginV0 {
+    @Override
+    public Identifier getPluginIdentifier() {
+        return UplandsMod.id("uplands_rei");
+    }
+
+    @Override
+    public void registerPluginCategories(RecipeHelper recipeHelper) {
+        recipeHelper.registerCategory(new AegisaltRecipeCategory());
+    }
+
+    @Override
+    public void registerRecipeDisplays(RecipeHelper recipeHelper) {
+        recipeHelper.registerRecipes(UplandsMod.id("plugins/aegisalt_infusions"), AegisaltRecipe.class, AegisaltRecipeDisplay::new);
+    }
+
+    @Override
+    public void registerOthers(RecipeHelper recipeHelper) {
+        recipeHelper.registerWorkingStations(UplandsMod.id("plugins/aegisalt_infusions"), EntryStack.create(MachineRegistry.aegisaltInfuser().block()));
+    }
+
+    @Override
+    public SemanticVersion getMinimumVersion() throws VersionParsingException {
+        return SemanticVersion.parse("3.0-pre");
+    }
+}
