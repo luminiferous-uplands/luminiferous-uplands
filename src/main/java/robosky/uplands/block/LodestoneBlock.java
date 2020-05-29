b4061620be0faa08f0d5579070e87f546bc28d31
Thalia Nero <chrisvn00@gmail.com>
2020-05-28 21:59:30 -0400

    Revert "After taking a long nap, I don't see the point of the direction property"
    This reverts commit 0dc6a7a04da91b4f5cf74fe9fa03dbe24db08211.

--- src/main/java/robosky/uplands/block/LodestoneBlock.java
+++ src/main/java/robosky/uplands/block/LodestoneBlock.java
@@ -3,7 +3,6 @@ package robosky.uplands.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
-import net.minecraft.block.SandBlock;
 import net.minecraft.client.network.DebugRendererInfoManager;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.item.ItemPlacementContext;
@@ -21,22 +20,36 @@ import java.util.Random;
 
 public class LodestoneBlock extends Block {
 
+    private static final Property<Integer> DISTANCE = IntProperty.of("distance", 0, 4);
+
     public LodestoneBlock(Settings settings) {
         super(settings);
     }
 
     @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE);
+    }
+
+    @Override
     public void scheduledTick(BlockState blockState, ServerWorld serverWorld, BlockPos blockPos, Random random) {
-        if (FallingBlock.canFallThrough(serverWorld.getBlockState(blockPos.down())) && blockPos.getY() >= 0) {
-            FallingBlockEntity fallingEntity = new FallingBlockEntity(
-                    serverWorld,
-                    blockPos.getX() + 0.5,
-                    blockPos.getY(),
-                    blockPos.getZ() + 0.5,
-                    serverWorld.getBlockState(blockPos));
-            fallingEntity.setHurtEntities(true);
-            serverWorld.spawnEntity(fallingEntity);
+        if (blockState.get(DISTANCE) == 4) {
+            if (FallingBlock.canFallThrough(serverWorld.getBlockState(blockPos.down())) && blockPos.getY() >= 0) {
+                FallingBlockEntity fallingEntity = new FallingBlockEntity(
+                        serverWorld,
+                        blockPos.getX() + 0.5,
+                        blockPos.getY(),
+                        blockPos.getZ() + 0.5,
+                        blockState.with(DISTANCE, 0));
+                fallingEntity.setHurtEntities(true);
+                serverWorld.spawnEntity(fallingEntity);
+            }
         }
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+         return this.getDefaultState().with(DISTANCE, this.updatedDistance(context.getWorld(), context.getBlockPos()));
     }
 
     @Override
@@ -46,12 +59,38 @@ public class LodestoneBlock extends Block {
 
     @Override
     public BlockState getStateForNeighborUpdate(BlockState blockState, Direction direction, BlockState blockState2, IWorld iWorld, BlockPos blockPos, BlockPos blockPos2) {
-        iWorld.getBlockTickScheduler().schedule(blockPos, this, getTickRate(iWorld));
-        return super.getStateForNeighborUpdate(blockState, direction, blockState2, iWorld, blockPos, blockPos2);
+        return blockState.with(DISTANCE, this.updatedDistance(iWorld, blockPos));
     }
 
     @Override
     public void neighborUpdate(BlockState blockState, World world, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl) {
         world.getBlockTickScheduler().schedule(blockPos, this, 1);
+    }
+
+    private int updatedDistance(BlockView world, BlockPos pos) {
+        for (Direction dir : Direction.values()) {
+            BlockState neighbor = world.getBlockState(pos.offset(dir));
+            if (dir == Direction.DOWN) {
+                if (neighbor.getBlock() == this) {
+                    neighbor.get(DISTANCE);
+                } else if (!FallingBlock.canFallThrough(neighbor)) {
+                    return 0;
+                } else {
+                    return 4;
+                }
+            } else {
+                if (neighbor.getBlock() == this) {
+                    return neighbor.get(DISTANCE) + 1;
+                }
+                else if (this.isFullOpaque(neighbor, world, pos.offset(dir))) {
+                    return 1;
+                }
+                else {
+                    return 4;
+                }
+            }
+        }
+
+        return 0;
     }
 }
